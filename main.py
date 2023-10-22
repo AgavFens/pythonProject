31613195c2f679f1e4173062bed8493be24db569
Agav <fensagav@gmail.com>
2023-10-22 19:33:22 +0300

    gamee

--- main.py
+++ main.py
@@ -1,70 +1,93 @@
-import math
+# Создаем пустой словарь для хранения заметок
+notes = {}
+
+# Функция для добавления заметки
+def add_note():
+    title = input("Введите название заметки: ")
+    content = input("Введите текст заметки: ")
+    notes[title] = content
+    print("Заметка успешно добавлена!")
+
+# Функция для удаления заметки
+def delete_note():
+    if not notes:
+        print("Список заметок пуст.")
+        return
+
+    print("Список доступных заметок:")
+    for title in notes.keys():
+        print(title)
+
+    title_to_delete = input("Введите название заметки для удаления: ")
+    if title_to_delete in notes:
+        del notes[title_to_delete]
+        print("Заметка успешно удалена.")
+    else:
+        print("Заметка с таким названием не найдена.")
+
+# Функция для вывода списка заметок
+def list_notes():
+    if not notes:
+        print("Список заметок пуст.")
+    else:
+        print("Список заметок:")
+        for title, content in notes.items():
+            print(f"{title}: {content}")
+
+# Функция для определения сезона по номеру месяца
+def get_season(month):
+    seasons = {
+        12: ("зимы", "За окном падал белый снег"),
+        1: ("зимы", "За окном падал белый снег"),
+        2: ("зимы", "За окном падал белый снег"),
+        3: ("весны", "Птицы пели прекрасные песни"),
+        4: ("весны", "Птицы пели прекрасные песни"),
+        5: ("весны", "Птицы пели прекрасные песни"),
+        6: ("лета", "Солнце светило ярче чем когда-либо"),
+        7: ("лета", "Солнце светило ярче чем когда-либо"),
+        8: ("лета", "Солнце светило ярче чем когда-либо"),
+        9: ("осени", "Урожай был невероятным"),
+        10: ("осени", "Урожай был невероятным"),
+        11: ("осени", "Урожай был невероятным"),
+    }
+
+    try:
+        month = int(month)
+        if month in seasons:
+            season, description = seasons[month]
+            return f"Вы родились в месяце {season}. {description}"
+        else:
+            return "Неверный номер месяца."
+    except ValueError:
+        return "Некорректный ввод. Введите номер месяца (1-12)."
+
+# Главная функция приложения
 def main():
-    print("Инженерный калькулятор")
     while True:
-        print("Доступные операции:")
-        print("1. Сложение")
-        print("2. Вычитание")
-        print("3. Умножение")
-        print("4. Деление")
-        print("5. Возведение в степень")
-        print("6. Квадратный корень")
-        print("7. Факториал")
-        print("8. Синус")
-        print("9. Косинус")
-        print("10. Тангенс")
-        print("11. Выход")
-
-        choice = input("Выберите операцию (1/2/3/4/5/6/7/8/9/10/11): ")
-
-        if choice == '11':
-            print("Пока.")
-            break
-
-        if choice not in ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'):
-            print("только с 1 до 11, дальше пути нет")
-            continue
-
-        if choice in ('1', '2', '3', '4', '5'):
-            try:
-                num1 = float(input("Введите первое число: "))
-                num2 = float(input("Введите второе число: "))
-            except ValueError:
-                print("ЧИСЛА ВВОДИ!!!")
-                continue
+        print("\nВыберите действие:")
+        print("1. Добавить заметку")
+        print("2. Удалить заметку")
+        print("3. Вывести список заметок")
+        print("4. Определить сезон по номеру месяца")
+        print("5. Выход")
+        choice = input("Введите номер действия: ")
 
         if choice == '1':
-            result = num1 + num2
+            add_note()
         elif choice == '2':
-            result = num1 - num2
+            delete_note()
         elif choice == '3':
-            result = num1 * num2
+            list_notes()
         elif choice == '4':
-            if num2 == 0:
-                print("Ошибка: деление на ноль")
-                continue
-            result = num1 / num2
+            month = input("Введите номер месяца вашего рождения: ")
+            season_info = get_season(month)
+            print(season_info)
         elif choice == '5':
-            result = num1 ** num2
-        elif choice == '6':
-            if num1 < 0:
-                print("Ошибка: нельзя извлекать корень из отрицательного числа")
-                continue
-            result = math.sqrt(num1)
-        elif choice == '7':
-            if num1 < 0:
-                print("Ошибка: факториал определен только для неотрицательных целых чисел")
-                continue
-            result = math.factorial(int(num1))
-        elif choice == '8':
-            result = math.sin(num1)
-        elif choice == '9':
-            result = math.cos(num1)
-        elif choice == '10':
-            result = math.tan(num1)
-
-        print("Результат: {}".format(result))
-
+            print("Выход из приложения.")
+            break
+        else:
+            print("Нет такой команды. Пожалуйста, выберите действие из списка.")
 
 if __name__ == "__main__":
     main()
+(2 ПРАКТИЧЕСКАЯ ДИСТАНЦИОНКА)
